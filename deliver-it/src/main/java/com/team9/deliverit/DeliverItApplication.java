d74d7dd5d7db7b4beb04529c9fdf6595f444ae9d
kristiyanpd <kristiyanpd02@gmail.com>
2021-07-24 10:47:34 +0300

    Commit initial files.

---
+++ deliver-it/src/main/java/com/team9/deliverit/DeliverItApplication.java
@@ -0,0 +1,13 @@
+package com.team9.deliverit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliverItApplication.class, args);
+    }
+
+}
