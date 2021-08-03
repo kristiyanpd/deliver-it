3a78f5e8cc472ca2e1b572c368dfc0ba75d8ea58
kristiyanpd <kristiyanpd02@gmail.com>
2021-08-03 16:47:56 +0300

    Added Customer classes.

---
+++ deliver-it/src/main/java/com/team9/deliverit/services/mappers/CustomerModelMapper.java
@@ -0,0 +1,16 @@
+package com.team9.deliverit.services.mappers;
+
+import com.team9.deliverit.repositories.CustomerRepository;
+import com.team9.deliverit.repositories.PersonalDetailsRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerModelMapper {
+
+    private final
+    private final
+    private final CustomerRepository customerRepository;
+    private final PersonalDetailsRepository personalDetailsRepository;
+
+
+}
