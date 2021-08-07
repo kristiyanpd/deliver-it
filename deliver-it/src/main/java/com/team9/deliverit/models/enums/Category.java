e5791784dc4ce7a8424997a764fbed0dcb1e63e8
kristiyanpd <kristiyanpd02@gmail.com>
2021-08-07 15:39:36 +0300

    #13 Updated enums to the new version

--- deliver-it/src/main/java/com/team9/deliverit/models/enums/Category.java
+++ deliver-it/src/main/java/com/team9/deliverit/models/enums/Category.java
@@ -1,9 +1,22 @@
 package com.team9.deliverit.models.enums;
 
 public enum Category {
-    ELECTRONICS,
-    CLOTHING,
-    MEDICAL;
+
+    ELECTRONICS("ELECTRONICS"),
+    CLOTHING("CLOTHING"),
+    MEDICAL("MEDICAL");
+
+    public static final String INVALID_CATEGORY = "Pick up option should be ELECTRONICS, CLOTHING or MEDICAL";
+
+    private final String value;
+
+    Category(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 
     @Override
     public String toString() {
@@ -15,7 +28,14 @@ public enum Category {
             case ELECTRONICS:
                 return "Electronics";
             default:
-                throw new IllegalArgumentException("Category should be ELECTRONICS, CLOTHING or MEDICAL");
+                throw new IllegalArgumentException(INVALID_CATEGORY);
         }
     }
+
+    public static Category getEnum(String value) {
+        for(Category v : values())
+            if(v.getValue().equalsIgnoreCase(value)) return v;
+        throw new IllegalArgumentException(INVALID_CATEGORY);
+    }
+
 }
--- deliver-it/src/main/java/com/team9/deliverit/models/enums/Status.java
+++ deliver-it/src/main/java/com/team9/deliverit/models/enums/Status.java
@@ -1,11 +1,22 @@
 package com.team9.deliverit.models.enums;
 
-import com.team9.deliverit.exceptions.StatusNotFoundException;
-
 public enum Status {
-    PREPARING,
-    ON_THE_WAY,
-    COMPLETED;
+
+    PREPARING("PREPARING"),
+    ON_THE_WAY("ON_THE_WAY"),
+    COMPLETED("COMPLETED");
+
+    public static final String INVALID_STATUS = "Pick up option should be PREPARING, ON_THE_WAY or COMPLETED";
+
+    private final String value;
+
+    Status(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 
     @Override
     public String toString() {
@@ -17,7 +28,14 @@ public enum Status {
             case COMPLETED:
                 return "Completed";
             default:
-                throw new StatusNotFoundException("Status must be PREPARING, ON_THE_WAY or COMPLETED!");
+                throw new IllegalArgumentException(INVALID_STATUS);
         }
     }
+
+    public static Status getEnum(String value) {
+        for(Status v : values())
+            if(v.getValue().equalsIgnoreCase(value)) return v;
+        throw new IllegalArgumentException(INVALID_STATUS);
+    }
+
 }
