2fe47bfdee60e3e4ddbe533c01c782ebf76fdfcc
kristiyanpd <kristiyanpd02@gmail.com>
2021-08-11 14:10:09 +0300

    Added ParcelServiceImplTests

---
+++ deliver-it/src/test/java/com/team9/deliverit/services/ParcelServiceImplTests.java
@@ -0,0 +1,275 @@
+package com.team9.deliverit.services;
+
+import com.team9.deliverit.exceptions.UnauthorizedOperationException;
+import com.team9.deliverit.models.Parcel;
+import com.team9.deliverit.models.Shipment;
+import com.team9.deliverit.models.dtos.ParcelDisplayDto;
+import com.team9.deliverit.models.enums.PickUpOption;
+import com.team9.deliverit.repositories.contracts.ParcelRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static com.team9.deliverit.Helpers.*;
+import static org.mockito.ArgumentMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ParcelServiceImplTests {
+
+    @Mock
+    ParcelRepository mockRepository;
+
+    @InjectMocks
+    ParcelServiceImpl service;
+
+    @Test
+    public void getAll_Should_ReturnEmptyList_When_RepositoryEmpty() {
+        // Arrange
+        List<Parcel> list = new ArrayList<>();
+
+        Mockito.when(mockRepository.getAll())
+                .thenReturn(list);
+
+        // Act
+        List<ParcelDisplayDto> result = service.getAll(createMockEmployee());
+
+        // Assert
+        Assertions.assertEquals(0, result.size());
+    }
+
+    @Test
+    public void getAll_Should_ReturnList_When_RepositoryNotEmpty() {
+        // Arrange
+        List<Parcel> list = new ArrayList<>();
+        list.add(createMockParcel());
+
+        Mockito.when(mockRepository.getAll())
+                .thenReturn(list);
+
+        // Act
+        List<ParcelDisplayDto> result = service.getAll(createMockEmployee());
+
+        // Assert
+        Assertions.assertEquals(1, result.size());
+    }
+
+    @Test
+    public void getAll_Should_Throw_When_UnauthorizedUser() {
+        // Assert
+        Assertions.assertThrows(UnauthorizedOperationException.class,
+                () -> service.getAll(createMockCustomer()));
+    }
+
+    @Test
+    public void getById_Should_Throw_WhenUnauthorizedUser() {
+        // Arrange
+        var mockParcel = createMockParcel();
+        var mockCustomer = createMockCustomer();
+        mockCustomer.setId(2);
+
+        // Act
+        Mockito.when(mockRepository.getById(Mockito.anyInt()))
+                        .thenReturn(mockParcel);
+
+        Assertions.assertThrows(UnauthorizedOperationException.class,
+                () -> service.getById(1, mockCustomer));
+    }
+
+    @Test
+    public void getById_Should_ReturnShipment_When_MatchExist() {
+        // Arrange
+        var mockParcel = createMockParcel();
+        Mockito.when(mockRepository.getById(anyInt()))
+                .thenReturn(mockParcel);
+        // Act
+        var result = service.getById(1, createMockEmployee());
+
+        // Assert
+        Assertions.assertEquals(1, result.getId());
+        Assertions.assertEquals(mockParcel.getWeight(), result.getWeight());
+    }
+
+    @Test
+    public void create_Should_Call_Repository_When_ParcelIsValid() {
+
+        var mockParcel = createMockParcel();
+
+        // Act
+        service.create(mockParcel, createMockEmployee());
+
+        // Assert
+        Mockito.verify(mockRepository, Mockito.times(1))
+                .create(Mockito.any(Parcel.class));
+    }
+
+    @Test
+    public void create_Should_Throw_When_UnauthorizedUser() {
+        // Assert
+        Assertions.assertThrows(UnauthorizedOperationException.class,
+                () -> service.create(createMockParcel(), createMockCustomer()));
+    }
+
+    @Test
+    public void update_Should_Call_Repository_When_ParcelIsValid() {
+
+        var mockParcel = createMockParcel();
+
+        service.update(mockParcel, createMockEmployee());
+        // Assert
+        Mockito.verify(mockRepository, Mockito.times(1))
+                .update(Mockito.any(Parcel.class));
+    }
+
+    @Test
+    public void update_Should_Throw_When_UnauthorizedUser() {
+        // Assert
+        Assertions.assertThrows(UnauthorizedOperationException.class,
+                () -> service.update(createMockParcel(), createMockCustomer()));
+    }
+
+    @Test
+    public void delete_Should_Throw_When_UserNotEmployee() {
+
+        Assertions.assertThrows(UnauthorizedOperationException.class,
+                () -> service.delete(1, createMockCustomer()));
+
+    }
+
+    @Test
+    public void delete_Should_Call_Repository_When_UserValid() {
+
+        service.delete(1, createMockEmployee());
+
+        // Assert
+        Mockito.verify(mockRepository, Mockito.times(1))
+                .delete(anyInt());
+
+    }
+
+    @Test
+    public void filter_Calls_Repository_When_UserNotEmployee() {
+        service.filter(Optional.empty(), Optional.empty(), Optional.empty(),
+                Optional.empty(), Optional.empty(), createMockCustomer());
+
+        // Assert
+        Mockito.verify(mockRepository, Mockito.times(1))
+                .filter(Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.of(1));
+    }
+
+    @Test
+    public void filter_Calls_Repository_When_UserIsValid() {
+
+        service.filter(Optional.empty(), Optional.empty(), Optional.empty(),
+                Optional.empty(), Optional.empty(), createMockEmployee());
+
+        // Assert
+        Mockito.verify(mockRepository, Mockito.times(1))
+                .filter(Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty());
+    }
+
+    @Test
+    public void sort_Should_Call_Repository_When_UserNotEmployee() {
+        service.sort(Optional.empty(), Optional.empty(), Optional.empty(), createMockCustomer());
+
+        // Assert
+        Mockito.verify(mockRepository, Mockito.times(1))
+                .sort(Optional.empty(), Optional.empty(), Optional.of(1));
+    }
+
+    @Test
+    public void sort_Should_Call_Repository_When_UserIsEmployee() {
+
+        service.sort(Optional.empty(), Optional.empty(), Optional.empty(), createMockEmployee());
+
+        // Assert
+        Mockito.verify(mockRepository, Mockito.times(1))
+                .sort(Optional.empty(), Optional.empty(), Optional.empty());
+    }
+
+    @Test
+    public void getAllUserParcels_Should_ReturnAll_UserParcels() {
+
+        var mockUser = createMockCustomer();
+        var mockParcel = createMockParcel();
+
+        List<Parcel> list = new ArrayList<>();
+        list.add(mockParcel);
+
+        Mockito.when(mockRepository.getAllUserParcels(Mockito.anyInt()))
+                .thenReturn(list);
+
+        var result = service.getAllUserParcels(mockUser);
+
+        Assertions.assertEquals(result.get(0).getId(), list.get(0).getId());
+        Assertions.assertEquals(result.get(0).getCategory(), list.get(0).getCategory());
+    }
+
+    @Test
+    public void getStatusOfParcel_Should_Throw_When_UserNotOwner() {
+        var mockParcel = createMockParcel();
+        var mockUser = createMockCustomer();
+        mockUser.setId(3);
+
+        Mockito.when(mockRepository.getById(Mockito.anyInt()))
+                        .thenReturn(mockParcel);
+
+        Assertions.assertThrows(UnauthorizedOperationException.class,
+                () -> service.getStatusOfParcel(mockUser, mockParcel.getId()));
+    }
+
+    @Test
+    public void getStatusOfParcel_Should_ReturnStatus_When_UserIsOwner() {
+        var mockParcel = createMockParcel();
+        var mockUser = createMockCustomer();
+        mockParcel.setUser(mockUser);
+
+        Mockito.when(mockRepository.getById(anyInt()))
+                .thenReturn(mockParcel);
+
+        Mockito.when(mockRepository.getStatusOfParcel(anyInt()))
+                .thenReturn(mockParcel.getShipment().getStatus().toString());
+
+        var result = service.getStatusOfParcel(mockUser, mockParcel.getId());
+
+        Assertions.assertEquals(result, mockParcel.getShipment().getStatus().toString());
+    }
+
+    @Test
+    public void updatePickUpOption_Should_Throw_When_UserNotOwner() {
+        var mockParcel = createMockParcel();
+        var mockUser = createMockCustomer();
+        mockUser.setId(3);
+
+        Mockito.when(mockRepository.getById(Mockito.anyInt()))
+                .thenReturn(mockParcel);
+
+        Assertions.assertThrows(UnauthorizedOperationException.class,
+                () -> service.updatePickUpOption(mockUser, mockParcel.getId(), "DELIVER_TO_ADDRESS"));
+    }
+
+    //TODO
+//    @Test
+//    public void updatePickUpOption_Should_ReturnStatus_When_UserIsOwner() {
+//        var mockParcel = createMockParcel();
+//        var mockUser = createMockCustomer();
+//        mockParcel.setUser(mockUser);
+//
+//        Mockito.when(mockRepository.updatePickUpOption(anyInt(), PickUpOption.DELIVER_TO_ADDRESS))
+//                .thenReturn(mockParcel);
+//
+//        service.updatePickUpOption(mockUser, mockParcel.getId(), eq("DELIVER_TO_ADDRESS"));
+//
+//        // Assert
+//        Mockito.verify(mockRepository, Mockito.times(1))
+//                .updatePickUpOption(1, PickUpOption.DELIVER_TO_ADDRESS);
+//    }
+
+}
