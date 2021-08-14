1caa95da3b4841d92a27b1989e170381d34c8db9
kristiyanpd <kristiyanpd02@gmail.com>
2021-08-14 13:10:00 +0300

    Removed try/catch from AuthenticationHelper

--- deliver-it/src/main/java/com/team9/deliverit/controllers/utils/AuthenticationHelper.java
+++ deliver-it/src/main/java/com/team9/deliverit/controllers/utils/AuthenticationHelper.java
@@ -1,13 +1,11 @@
 package com.team9.deliverit.controllers.utils;
 
-import com.team9.deliverit.exceptions.EntityNotFoundException;
+import com.team9.deliverit.exceptions.UnauthorizedOperationException;
 import com.team9.deliverit.models.User;
 import com.team9.deliverit.services.contracts.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class AuthenticationHelper {
@@ -23,15 +21,9 @@ public class AuthenticationHelper {
 
     public User tryGetUser(HttpHeaders headers) {
         if (!headers.containsKey(AUTHORIZATION_HEADER_NAME)) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,
-                    "The requested resource requires authentication!");
+            throw new UnauthorizedOperationException("The requested resource requires authentication!");
         }
-
-        try {
-            String email = headers.getFirst(AUTHORIZATION_HEADER_NAME);
-            return userService.getByEmail(email);
-        } catch (EntityNotFoundException e) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid username!");
-        }
+        String email = headers.getFirst(AUTHORIZATION_HEADER_NAME);
+        return userService.getByEmail(email);
     }
 }
