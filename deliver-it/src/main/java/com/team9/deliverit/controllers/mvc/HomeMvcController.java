a43875a5e93f600a5de416bb8c86e4c8981d25d3
Kamen <nahtata.kg@gmail.com>
2021-08-25 20:12:37 +0300

    implemented index.html with theme

--- deliver-it/src/main/java/com/team9/deliverit/controllers/mvc/HomeMvcController.java
+++ deliver-it/src/main/java/com/team9/deliverit/controllers/mvc/HomeMvcController.java
@@ -1,15 +1,36 @@
 package com.team9.deliverit.controllers.mvc;
 
+import com.team9.deliverit.services.contracts.UserService;
+import com.team9.deliverit.services.contracts.WarehouseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+import static com.team9.deliverit.config.ApplicationConstants.CURRENT_USER_SESSION_KEY;
 
 @Controller
 @RequestMapping("/")
 public class HomeMvcController extends BaseAuthenticationController {
 
+    private final UserService service;
+
+    @Autowired
+    public HomeMvcController(UserService service) {
+        this.service = service;
+    }
+
+    @ModelAttribute("customersCount")
+    public String customersCount() {
+        return String.valueOf(service.countCustomers());
+    }
+
     @GetMapping
     public String showHomePage() {
         return "index";
     }
+
 }
